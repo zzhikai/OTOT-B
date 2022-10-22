8f8f943f80f705b34b6d3973524cbb5e0d7b0b1c
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-10-23 07:41:48 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -62,100 +62,100 @@ describe("Contacts", function (done) {
           done();
         });
     });
-    // Test to create existing contact
-    // it("should fail to create an existing contact", function (done) {
-    //   this.timeout(2000);
-    //   const contact = new Contact({
-    //     name: "John Doe",
-    //     email: "John@email.com",
-    //     phoneNumber: "555-555-5555",
-    //   });
-    //   contact.save((err, contact) => {
-    //     chai
-    //       .request(app)
-    //       .post("/api/contacts")
-    //       .send(contact)
-    //       .end((err, res) => {
-    //         res.should.have.status(400);
-    //         res.body.should.have
-    //           .property("message")
-    //           .eql("Contact already exists");
-    //         done();
-    //       });
-    //   });
-    // });
-    // Test the /PUT route
-    describe("PUT /", () => {
-      // Test to update a contact
-      it("should update a contact's phone number", function (done) {
-        this.timeout(5000);
-        const contact = new Contact({
-          name: "John Doe",
-          email: "John@email.com",
-          phoneNumber: "555-555-5555",
-        });
-        contact.save((err, contact) => {
-          chai
-            .request(app)
-            .put("/api/contacts")
-            .send({
-              name: "John Doe",
-              email: "John@email.com",
-              phoneNumber: "555-555-5556",
-            })
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a("object");
-              res.body.should.have
-                .property("message")
-                .eql("Updated contact number to 555-555-5556 successfully!");
-              done();
-            });
-        });
+  });
+  // Test the /PUT route
+  describe("PUT /", () => {
+    // Test to update a contact
+    it("should update a contact's phone number", function (done) {
+      this.timeout(5000);
+      const contact = new Contact({
+        name: "John Doe",
+        email: "John@email.com",
+        phoneNumber: "555-555-5555",
       });
-    });
-
-    // Test the /DELETE route
-    describe("DELETE /", () => {
-      // Test to delete a contact
-      it("should delete a contact", function (done) {
-        const contact = new Contact({
-          name: "John Doe",
-          email: "John@email.com",
-          phoneNumber: "555-555-5555",
-        });
-        contact.save((err, contact) => {
-          chai
-            .request(app)
-            .delete("/api/contacts/" + contact._id)
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a("object");
-              res.body.should.have
-                .property("message")
-                .eql("Deleted contact successfully!");
-              res.body.should.have.property("name").eql(contact.name);
-              done();
-            });
-        });
+      contact.save((err, contact) => {
+        chai
+          .request(app)
+          .put("/api/contacts")
+          .send({
+            name: "John Doe",
+            email: "John@email.com",
+            phoneNumber: "555-555-5556",
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have
+              .property("message")
+              .eql("Updated contact number to 555-555-5556 successfully!");
+            done();
+          });
       });
-
-      // Test to delete non existent contact
-      // it("Should indicate contact not exist", function (done) {
-      //   this.timeout(5000);
-      //   const id = "63545b4e486381e53eb6c7f1";
-      //   chai
-      //     .request(app)
-      //     .delete("/api/contacts/" + id)
-      //     .end((err, res) => {
-      //       res.should.have.status(404);
-      //       res.body.should.be.a("object");
-      //       res.body.should.have
-      //         .property("message")
-      //         .eql("Contact does not exist");
-      //       done();
-      //     });
-      // });
     });
   });
+  // Test to create existing contact
+  // it("should fail to create an existing contact", function (done) {
+  //   this.timeout(2000);
+  //   const contact = new Contact({
+  //     name: "John Doe",
+  //     email: "John@email.com",
+  //     phoneNumber: "555-555-5555",
+  //   });
+  //   contact.save((err, contact) => {
+  //     chai
+  //       .request(app)
+  //       .post("/api/contacts")
+  //       .send(contact)
+  //       .end((err, res) => {
+  //         res.should.have.status(400);
+  //         res.body.should.have
+  //           .property("message")
+  //           .eql("Contact already exists");
+  //         done();
+  //       });
+  //   });
+  // });
+
+  // Test the /DELETE route
+  describe("DELETE /", () => {
+    // Test to delete a contact
+    it("should delete a contact", function (done) {
+      const contact = new Contact({
+        name: "John Doe",
+        email: "John@email.com",
+        phoneNumber: "555-555-5555",
+      });
+      contact.save((err, contact) => {
+        chai
+          .request(app)
+          .delete("/api/contacts/" + contact._id)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have
+              .property("message")
+              .eql("Deleted contact successfully!");
+            res.body.should.have.property("name").eql(contact.name);
+            done();
+          });
+      });
+    });
+  });
+
+  // Test to delete non existent contact
+  // it("Should indicate contact not exist", function (done) {
+  //   this.timeout(5000);
+  //   const id = "63545b4e486381e53eb6c7f1";
+  //   chai
+  //     .request(app)
+  //     .delete("/api/contacts/" + id)
+  //     .end((err, res) => {
+  //       res.should.have.status(404);
+  //       res.body.should.be.a("object");
+  //       res.body.should.have
+  //         .property("message")
+  //         .eql("Contact does not exist");
+  //       done();
+  //     });
+  // });
 });
