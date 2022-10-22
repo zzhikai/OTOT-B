0882df7076b13311267de8f3baa40ede035b6499
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-10-23 07:23:56 +0800

    add timeout

--- test/test.js
+++ test/test.js
@@ -13,6 +13,7 @@ if (process.env.NODE_ENV != "test") {
   console.log("NODE_ENV is set to: ", process.env.NODE_ENV);
 }
 describe("Contacts", function (done) {
+  this.timeout(30000);
   beforeEach((done) => {
     // Before each test we empty the database
     Contact.deleteOne({}, (err) => {
